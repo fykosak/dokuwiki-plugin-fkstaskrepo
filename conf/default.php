12c460937634c93433e8a93c3ec832888ac92e5a
Mišo <miso@fykos.cz>
2014-04-12 13:08:29 +0200

    [conf]

---
+++ conf/default.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the fksdbexport plugin
+ *
+ * @author Michal Koutný <michal@fykos.cz>
+ */
+
+
+$conf['taskrepo'] = 'data/pages/ulohy/year@Y@/serie@S@/';
+$conf['solutionrepo'] = 'data/pages/ulohy/year@Y@/serie@S@/';
---
+++ conf/metadata.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Options for the fksdbexport plugin
+ *
+ * @author Michal Koutný <michal@fykos.cz>
+ */
+
+
+$meta['taskrepo'] = array('string');
+$meta['solutionrepo'] = array('string');
+
+
