db6037cf2ace3b8751a80496d487093e5526d59d
Mišo <miso@fykos.cz>
2014-04-13 13:08:44 +0200

    [task repo]

--- syntax/fkstaskrepo.php
+++ syntax/fkstaskrepo.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Michal Červeňák     
+ * @author     Radka štefaníková
+ */
+// must be run within Dokuwiki
 if (!defined('DOKU_INC')) {
     die();
 }
@@ -33,30 +39,15 @@ class syntax_plugin_fksproblems_fkstaskrepo extends DokuWiki_Syntax_Plugin {
      * Handle the match
      */
     public function handle($match, $state, $pos, Doku_Handler &$handler) {
-        $probsno = array();
         $probsno = preg_split('/-/', substr($match, 14, -2));
         $taskfileurl = str_replace('@Y@', $probsno[0], $this->getConf('taskrepo'));
-        $taskfileurl = str_replace('@S@', $probsno[1], $taskfileurl);
+        $taskfileurl=str_replace('@S@', $probsno[1], $taskfileurl);
         $probstask = preg_split('/===/', io_readFile("$taskfileurl.txt", FALSE));
         $to_page.='<div>';
         $to_page.=p_render("xhtml", p_get_instructions('==== ' . $probstask[2 * $probsno[2] - 1] . ' ==== '), $info);
         $to_page.=p_render("xhtml", p_get_instructions($probstask[2]), $info);
         $to_page.='</div>';
-        /* } else {
-          $to_page.='<div class="fksprobtask" id="fksprobtask'.$i/2 .'">';
-          $to_page.=p_render("xhtml", p_get_instructions($probstask[$i]), $info);
-          $to_page.='</div>';
-          $to_page.= '<h3 onclick="viewsolution('.$i/2 .')"> zobraz riešenie </h3>';
-          $to_page.='<div class="fksprobsol" id="fksprobsol'.$i/2 .'" style="display:none">';
-          if(!$probssolution[$i]==NUll){
-          $to_page.= p_render("xhtml", p_get_instructions($probssolution[$i]), $info);
-          }else{
-          $to_page.= 'Rešení ešte nebolo nahrané';
-          }
-
-          $to_page.='</div>';
-          } */
-
+        
         return array($state, array($to_page));
     }
 
