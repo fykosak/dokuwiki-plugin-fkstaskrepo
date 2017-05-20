c31dd6dba811917c557cc4ecb6c01869277ecd20
Mišo <miso@fykos.cz>
2017-05-20 15:48:11 +0200

    fix config

--- conf/metadata.php
+++ conf/metadata.php
@@ -7,11 +7,11 @@ $meta['page_path_mask_en'] = ['string'];
 
 $meta['series_template'] = ['string'];
 
-$conf['brochure_path_cs'] = ['string'];
-$conf['brochure_path_en'] = ['string'];
+$meta['brochure_path_cs'] = ['string'];
+$meta['brochure_path_en'] = ['string'];
 
-$conf['solution_path_cs'] = ['string'];
-$conf['solution_path_en'] = ['string'];
+$meta['solution_path_cs'] = ['string'];
+$meta['solution_path_en'] = ['string'];
 
-$conf['archive_path_cs'] = ['string'];
-$conf['archive_path_en'] = ['string'];
+$meta['archive_path_cs'] = ['string'];
+$meta['archive_path_en'] = ['string'];
